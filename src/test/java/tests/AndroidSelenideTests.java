61baa11ebb34f03f3d0546ab711bb6fe2b3478ed
0yanna <yannaosodoeva@outlook.com>
2022-05-11 21:44:58 +0400

    add test and another files

--- src/test/java/tests/AndroidSelenideTests.java
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -4,27 +4,29 @@ import com.codeborne.selenide.Configuration;
 import driver.BrowserstackMobileDriver;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
+@Tag("selenide_android")
 public class AndroidSelenideTests {
     @Test
     @DisplayName("Successful search in wikipedia android app")
     void searchTest() throws MalformedURLException, InterruptedException {
-        Configuration.browser = BrowserstackMobileDriver.class.getName();
-        Configuration.startMaximized = false;
-        Configuration.browserSize = null;
-        Configuration.timeout = 10000;
 
-        open();
-        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
-        $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container")).shouldHave(sizeGreaterThan(0));
+        step("Type search", () -> {
+            open();
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
 
-        closeWebDriver();
+        });
+        step("Verify content found", () ->
+            $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container")).shouldHave(sizeGreaterThan(0)));
+
 }
 }
